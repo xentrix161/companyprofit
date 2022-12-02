d0c03988216d6a43a05d7646d862087c7152716d
Filip Kosmeľ <filip.kosmel@blueweb.sk>
2022-12-02 11:56:52 +0100

    BanknotesFacade.php refactored

--- app/Model/Facades/BanknotesFacade.php
+++ app/Model/Facades/BanknotesFacade.php
@@ -6,41 +6,58 @@ use Nette;
 
 final class BanknotesFacade
 {
+    /**
+     * Get the minimal possible banknotes from entered value
+     *
+     * @param $profit
+     * @return array
+     */
     public function getBanknotesCounts($profit): array
     {
-        $notes = array('500', '200', '100', '50', '20', '10', '5', '2', '1', '0.5', '0.2', '0.1', '0.05', '0.02', '0.01');
-        $noteCounter = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-        $countNotes = count($noteCounter);
+        $noteValues = array('500', '200', '100', '50', '20', '10', '5', '2', '1', '0.5', '0.2', '0.1', '0.05', '0.02', '0.01');
+        $banknotesCounter = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        $noteValuesCount = count($banknotesCounter);
+        $banknotes = [];
 
-        for ($i = 0; $i < $countNotes; $i++)
-        {
-            if ($profit >= (float)$notes[$i])
-            {
-                $noteCounter[$i] = intval($profit / $notes[$i]);
-                $profit = $profit - $noteCounter[$i] * $notes[$i];
+        for ($i = 0; $i < $noteValuesCount; $i++) {
+            if ($profit >= (float)$noteValues[$i]) {
+                $banknotesCounter[$i] = intval($profit / $noteValues[$i]);
+                $profit = $profit - $banknotesCounter[$i] * $noteValues[$i];
             }
         }
 
-        $banknotes = [];
-        for ($i = 0; $i < $countNotes; $i++)
+        for ($i = 0; $i < $noteValuesCount; $i++)
         {
-            $banknotes[$notes[$i]] = $noteCounter[$i];
+            $banknotes[$noteValues[$i]] = $banknotesCounter[$i];
         }
 
         return $banknotes;
     }
 
-    public function getNumberOfDecimals($number)
+    /**
+     * Get count of decimals from entered number
+     *
+     * @param $number
+     * @return int
+     */
+    public function getNumberOfDecimals($number): int
     {
         return (int)strpos(strrev((string)$number), ".");
     }
 
-    public function getBackCalcValue($totalBanknotes)
+    /**
+     * Get checksum from paid out banknotes. Banknotes must be in assoc array (key = value of banknote,
+     * value = banknote count)
+     *
+     * @param $totalBanknotes
+     * @return float|int
+     */
+    public function getBackCalc($totalBanknotes): float|int
     {
-        $totalValue = 0;
+        $checksum = 0;
         foreach ($totalBanknotes as $value => $count) {
-            $totalValue += (float)$value * $count;
+            $checksum += (float)$value * $count;
         }
-        return $totalValue;
+        return $checksum;
     }
 }
